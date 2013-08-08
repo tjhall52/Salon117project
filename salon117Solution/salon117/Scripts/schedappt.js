2ae8544c8b17dd438894d00628784197bed3f7c5
Tracey Hall <tracey.hallwright@gmail.com>
2013-08-07 20:07:18 -0400

    Final Project - CS6150 Web Development

---
+++ salon117Solution/salon117/Scripts/schedappt.js
@@ -0,0 +1,9 @@
+﻿
+//createAppt - writes to local storage
+function createAppt(treatment) {
+    var appt = JSON.parse(window.localStorage.getItem(treatment));
+    if (!treatment) {
+        return [];
+    }
+   
+}
