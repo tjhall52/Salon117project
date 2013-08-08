ed8067ed5da9fcacf335c89e98cffeb7b8127f80
Tracey Hall <tracey.hallwright@gmail.com>
2013-08-07 20:07:49 -0400

    Revert "Final Project - CS6150 Web Development"
    This reverts commit 2ae8544c8b17dd438894d00628784197bed3f7c5.

--- salon117Solution/salon117/Scripts/schedappt.js
+++
@@ -1,9 +0,0 @@
-﻿
-//createAppt - writes to local storage
-function createAppt(treatment) {
-    var appt = JSON.parse(window.localStorage.getItem(treatment));
-    if (!treatment) {
-        return [];
-    }
-   
-}
